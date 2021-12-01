622d4fbbb2db3fb991f096017940dd591de81843
Brandon Ros <brandonros1@gmail.com>
2021-12-01 16:32:49 -0500

    one off -x86 tag

--- lib/index.ts
+++ lib/index.ts
@@ -23,7 +23,7 @@ async function download(
   { tag, name }: Remote,
   local: string
 ): Promise<boolean> {
-  const url = `https://github.com/brandonros/pkg-fetch/releases/download/${tag}/${name}`;
+  const url = `https://github.com/brandonros/pkg-fetch/releases/download/${tag}-x86/${name}`;
 
   try {
     await downloadUrl(url, local);
